d5600da3de49d85d2d00196b2dc457547489c1c7
ARich <alecwrichardson@gmail.com>
2026-02-24 03:28:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -181,5 +181,7 @@ function updateChart(){
 
 function getRandomColor(){const letters='0123456789ABCDEF';let color='#';for(let i=0;i<6;i++)color+=letters[Math.floor(Math.random()*16)];return color;}
 
-// --------------- Init
-]);
+// --------------- Init -----------------
+showPage("workout"); renderWorkout(); renderHistory(); updateExerciseFilter(); updateChart();
+
+}); // <-- Close DOMContentLoaded
