33a5feeeee5001e0ca992e132b55232539dd78fb
ARich <alecwrichardson@gmail.com>
2026-02-26 00:37:02 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,15 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, query, orderBy } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-// 🔹 Replace these with your Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MSG_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyDPRS8sEsvVCuMl0DpM2xKlX_G_fEizzmE",
+  authDomain: "lift-tracker-8674a.firebaseapp.com",
+  projectId: "lift-tracker-8674a",
+  storageBucket: "lift-tracker-8674a.appspot.com",
+  messagingSenderId: "423500232178",
+  appId: "1:423500232178:web:5413feefa5da27997b676b",
+  measurementId: "G-V7SM6L4Z8M"
 };
 
 const app = initializeApp(firebaseConfig);
